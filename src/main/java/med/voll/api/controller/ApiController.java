ad9918ceb19440b4de05a7c7bb70111de59a9922
Maurício Ricardo <mauriciod37@gmail.com>
2024-01-05 06:10:20 -0300

    Primeiro Commit

---
+++ src/main/java/med/voll/api/controller/ApiController.java
@@ -0,0 +1,15 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ApiController {
+
+    @GetMapping
+    public String initialMessage(){
+        return "My firsttt Message";
+    }
+}
