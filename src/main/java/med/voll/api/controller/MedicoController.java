5485d8aa5a60e5c061015e8eaf3f87ff8ac352e3
Maurício Ricardo <43684435+mauriciocostaol@users.noreply.github.com>
2024-01-12 15:24:05 -0300

    Paginacao e log no console

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,9 +33,9 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> consultarMedicos() {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10) Pageable paginacao) {
 
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
